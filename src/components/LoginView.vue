109038d5679136fc8f65334538b1b685edc95b75
DevMAyman <eng.developer.mohamed.ayman@gmail.com>
2024-05-08 19:09:34 +0300

    Routing, form validation using Vulidate, and register

---
+++ src/components/LoginView.vue
@@ -0,0 +1,29 @@
+<!-- MyComponent.vue -->
+
+<template>
+  <div>
+    <p>Login</p>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello Vue!'
+    };
+  },
+  methods: {
+    changeMessage() {
+      this.message = 'Vue is awesome!';
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Scoped styles specific to this component */
+h1 {
+  color: blue;
+}
+</style>
