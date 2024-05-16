2a91a427006274b71f683bcd6381bf798113f6a1
hussein-elmlah <husseinelmlah11@gmail.com>
2024-05-16 11:56:49 +0300

    add filters on searchJob page

--- src/components/job/JobSearch.vue
+++ src/components/job/JobSearch.vue
@@ -1,164 +1,176 @@
 <template>
   <div class="container-fluid">
-    <!-- <h1>Job Search</h1> -->
-
-    <div class="row pt-2">
-      <div class="col-md-6">
-        <div class="input-group mb-2 shadow-sm">
-          <input
-            type="text"
-            class="form-control"
-            placeholder="Search term"
-            v-model="searchTerm"
-            @input="searchJobs"
-          />
-          <div class="input-group-append rounded-0">
-            <span class="input-group-text rounded-0">Search by:</span>
+    <div class="row pt-3 px-5">
+      <!-- Input fields -->
+      <div class="inputFields col-lg-6 col-xl-3 ps-5">
+        <div class="p-5 bg-body-emphasis border rounded-3 shadow-sm my-2">
+          <div class="gx-5 gy-3">
+            <h4 class="text-info-emphasis">Search</h4>
+            <div class="searchArea p-5 border rounded-3">
+              <input type="text" class="form-control mb-3" placeholder="Search term" v-model="searchTerm"
+                @input="searchJobs" />
+              <span class="d-block mb-2">Search by:</span>
+              <select class="form-select" v-model="searchBy" @change="searchJobs">
+                <option v-for="field in searchFields" :value="field">
+                  {{ field }}
+                </option>
+              </select>
+              <!-- Add more content here if needed -->
+            </div>
           </div>
-          <select class="form-select" v-model="searchBy" @change="searchJobs">
-            <option v-for="field in searchFields" :value="field">
-              {{ field }}
-            </option>
-          </select>
-        </div>
-      </div>
-    </div>
-    <!-- Display job list or loading indicator -->
-    <div v-if="loading">Loading...</div>
-    <div v-else>
-      <div class="row g-3">
-        <div v-for="job in joblist.jobs" :key="job.id" class="col-md-3">
-          <div class="card mb-4" style="height: 100%">
-            <div class="card-body d-flex flex-column">
-              <div
-                v-show="specifyRole('candidate')"
-                class="row align-items-center my-0"
-              >
-                <div class="col-2">
-                  <!-- Wrap the logo within the router-link -->
-                  <router-link
-                    :to="{
-                      name: 'CandidateJobDetails',
-                      params: { id: job.id },
-                    }"
-                  >
-                    <img
-                      :src="job.logo"
-                      alt="Company Logo"
-                      style="width: 70px; height: 70px"
-                      class="rounded-circle"
-                    />
-                  </router-link>
-                </div>
-                <div class="col-10">
-                  <!-- Wrap the title within the same router-link -->
-                  <router-link
-                    :to="{
-                      name: 'CandidateJobDetails',
-                      params: { id: job.id },
-                    }"
-                    class="text-decoration-none text-black"
-                  >
-                    <h5 class="card-title">{{ job.title }}</h5>
-                  </router-link>
-                </div>
-              </div>
-              <div
-                v-show="specifyRole('employer')"
-                class="row align-items-center my-0"
-              >
-                <div class="col-2">
-                  <!-- Wrap the logo within the router-link -->
-                  <router-link
-                    :to="{ name: 'EmployerJobDetails', params: { id: job.id } }"
-                  >
-                    <img
-                      :src="job.logo"
-                      alt="Company Logo"
-                      style="width: 70px; height: 70px"
-                      class="rounded-circle"
-                    />
-                  </router-link>
-                </div>
-                <div class="col-10">
-                  <!-- Wrap the title within the same router-link -->
-                  <router-link
-                    :to="{ name: 'EmployerJobDetails', params: { id: job.id } }"
-                    class="text-decoration-none text-black"
-                  >
-                    <h5 class="card-title">{{ job.title }}</h5>
-                  </router-link>
-                </div>
-              </div>
-              <div class="flex-grow-1 my-0">
-                <!-- <p class="card-text"><strong>Description:</strong> {{ job.description }}</p> -->
-                <!-- <p class="card-text"><strong>Responsibilities:</strong> {{ job.responsibilities }}</p> -->
-                <p class="card-text">
-                  <strong>Skills:</strong> {{ job.skills }}
-                </p>
-                <!-- <p class="card-text"><strong>Qualifications:</strong> {{ job.qualifications }}</p> -->
-                <p class="card-text">
-                  <strong>Salary Range:</strong> {{ job.salary_range }} $
-                </p>
-                <!-- <p class="card-text"><strong>Benefits:</strong> {{ job.benefits }}</p> -->
-                <p class="card-text">
-                  <strong>Location:</strong> {{ job.location }}
-                </p>
-                <p class="card-text">
-                  <strong>Work Type:</strong> {{ job.work_type }}
-                </p>
-                <p class="card-text mb-0">
-                  <strong>Application Deadline:</strong>
-                  {{ job.application_deadline.slice(0, 10) }}
-                </p>
-                <!-- <p class="card-text"><strong>Status:</strong> {{ job.status }}</p> -->
-                <!-- <p class="card-text"><strong>Created At:</strong> {{ job.created_at.slice(0, 10) }}</p>
-                <p class="card-text"><strong>Updated At:</strong> {{ job.updated_at.slice(0, 10) }}</p>                     -->
-              </div>
-              <!-- <button class="btn btn-primary mt-auto">View Details</button> -->
-              <router-link
-                v-show="specifyRole('candidate')"
-                :to="{ name: 'CandidateJobDetails', params: { id: job.id } }"
-                class="btn btn-primary mt-auto mb-1"
-                >View Details</router-link
-              >
-              <!-- <router-link v-show="specifyRole('employer')" :to="{ name: 'EmployerJobDetails', params: { id: job.id } }" class="btn btn-primary mt-auto mb-1">View Details</router-link> -->
-              <div class="d-flex justify-content-between">
-                <div class="col-6 pe-1">
-                  <router-link
-                    v-show="specifyRole('employer')"
-                    :to="{ name: 'updateJob', params: { id: job.id } }"
-                    class="w-100 btn btn-info mt-auto mb-1"
-                    >Update Job</router-link
-                  >
-                </div>
-                <div class="col-6 ps-1">
-                  <button
-                    v-show="specifyRole('employer')"
-                    class="w-100 btn btn-danger mt-auto mb-1"
-                    @click="deleteJob(job.id)"
-                  >
-                    Delete Job
-                  </button>
-                </div>
-              </div>
+
+          <div class="gx-5 gy-3 pt-5">
+            <h4 class="text-info-emphasis">Filters</h4>
+            <div class="searchArea p-5 border rounded-3">
+              <span class="d-block mb-2">Work Place:</span>
+              <select class="form-select" v-model="workPlace" @change="searchJobs">
+                <option value="" selected>Unselected</option>
+                <option value="remote">Remote</option>
+                <option value="on-site">On-site</option>
+                <option value="hybrid">Hybrid</option>
+              </select>
+              <!-- Add more content here if needed -->
             </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div v-if="totalCount > 0">
-      <Pagination
-        :currentPage="currentPage"
-        :totalPages="totalPages"
-        :totalCount="totalCount"
-        @page-changed="handlePageChange"
-      />
-    </div>
-    <div v-else class="row py-3">
-      <div class="col-md-12">
-        <div class="alert alert-warning" role="alert">No results found.</div>
+      <!-- Display job list or loading indicator -->
+      <div v-if="loading">Loading...</div>
+      <div v-else class="col-lg-6 col-xl-9">
+        <div class="row g-3">
+          <div v-for="job in joblist.jobs" :key="job.id" class="col-md-12 mt-3 overflow-hidden">
+            <div class="card mb-4 px-5" style="
+                height: 100%;
+                width: 90%;
+                margin: 10px auto;
+                box-shadow: 3px 3px 3px 5px #f3f3f3;
+              ">
+              <div class="card-body d-flex flex-column">
+                <div v-show="specifyRole('candidate')" class="row align-items-center my-0">
+                  <div class="col-10">
+                    <!-- Wrap the title within the same router-link -->
+                    <router-link :to="{
+                      name: 'CandidateJobDetails',
+                      params: { id: job.id },
+                    }" class="text-decoration-none text-black">
+                      <h2 class="card-title text-primary" style="font-size: 25px">
+                        {{ job.title }}
+                      </h2>
+                    </router-link>
+                  </div>
+                  <div class="col-2 overflow-hidden" style="text-align: center">
+                    <!-- Wrap the logo within the router-link -->
+                    <router-link :to="{
+                      name: 'CandidateJobDetails',
+                      params: { id: job.id },
+                    }"
+                    class=""
+                    >
+                      <img :src="job.logo" alt="Company Logo" style="width: 150px; height: 150px"
+                        class="rounded-circle pe-5" />
+                    </router-link>
+                  </div>
+                </div>
+                <div v-show="specifyRole('employer')" class="row align-items-center my-0">
+                  <div class="col-10">
+                    <!-- Wrap the title within the same router-link -->
+                    <router-link :to="{
+                      name: 'EmployerJobDetails',
+                      params: { id: job.id },
+                    }" class="text-decoration-none text-black">
+                      <h5 class="card-title text-primary" style="font-size: 25px">
+                        {{ job.title }}
+                      </h5>
+                    </router-link>
+                  </div>
+                  <div class="col-2 overflow-hidden">
+                    <!-- Wrap the logo within the router-link -->
+                    <router-link :to="{
+                      name: 'EmployerJobDetails',
+                      params: { id: job.id },
+                    }"
+                    class=""
+                    >
+                      <img :src="job.logo" alt="Company Logo" style="width: 150px; height: 150px"
+                        class="rounded-circle pe-5" />
+                    </router-link>
+                  </div>
+                </div>
+                <div class="flex-grow-1 my-0">
+                  <!-- <p class="card-text"><strong>Description:</strong> {{ job.description }}</p> -->
+                  <!-- <p class="card-text"><strong>Responsibilities:</strong> {{ job.responsibilities }}</p> -->
+                  <p class="card-text">
+                    <strong>Skills:</strong> {{ job.skills }}
+                  </p>
+                  <!-- <p class="card-text"><strong>Qualifications:</strong> {{ job.qualifications }}</p> -->
+                  <p class="card-text">
+                    <strong>Salary Range:</strong> {{ job.salary_range }} $
+                  </p>
+                  <!-- <p class="card-text"><strong>Benefits:</strong> {{ job.benefits }}</p> -->
+                  <p class="card-text">
+                    <strong>Location:</strong> {{ job.location }}
+                  </p>
+                  <p class="card-text">
+                    <strong>Work Type:</strong> {{ job.work_type }}
+                  </p>
+                  <div style="
+                      display: flex;
+                      justify-content: space-between;
+                      align-items: center;
+                    ">
+                    <div>
+                      <p class="card-text mb-0">
+                        <strong>Application Deadline:</strong>
+                        {{ job.application_deadline.slice(0, 10) }}
+                      </p>
+                    </div>
+
+                    <div>
+                      <div>
+                        <router-link v-show="specifyRole('candidate')" style="width: 150px" :to="{
+                          name: 'CandidateJobDetails',
+                          params: { id: job.id },
+                        }" class="btn btn-primary mt-auto mb-1">View Details</router-link>
+                      </div>
+                      <div class="d-flex justify-content-between" style="width: 220px">
+                        <div class="col-6 pe-1">
+                          <router-link v-show="specifyRole('employer')"
+                            :to="{ name: 'updateJob', params: { id: job.id } }"
+                            class="w-100 btn btn-info mt-auto mb-1">Update</router-link>
+                        </div>
+                        <div class="col-6 ps-1">
+                          <button v-show="specifyRole('employer')" class="w-100 btn btn-danger mt-auto mb-1"
+                            @click="deleteJob(job.id)">
+                            Delete
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <!-- <p class="card-text"><strong>Status:</strong> {{ job.status }}</p> -->
+                  <!-- <p class="card-text"><strong>Created At:</strong> {{ job.created_at.slice(0, 10) }}</p>
+                <p class="card-text"><strong>Updated At:</strong> {{ job.updated_at.slice(0, 10) }}</p>                     -->
+                </div>
+                <!-- <button class="btn btn-primary mt-auto">View Details</button> -->
+
+                <!-- <router-link v-show="specifyRole('employer')" :to="{ name: 'EmployerJobDetails', params: { id: job.id } }" class="btn btn-primary mt-auto mb-1">View Details</router-link> -->
+              </div>
+            </div>
+          </div>
+
+          <div v-if="totalCount > 0">
+            <Pagination :currentPage="currentPage" :totalPages="totalPages" :totalCount="totalCount"
+              @page-changed="handlePageChange" />
+          </div>
+          <div v-else class="row py-3">
+            <div class="col-md-12">
+              <div class="alert alert-warning" role="alert">
+                No results found.
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -185,6 +197,7 @@ export default {
       searchBy: "title",
       loading: false,
       searchFields: [],
+      workPlace: "",
       limit: 8,
       currentPage: 1,
       totalPages: 0,
@@ -210,7 +223,7 @@ export default {
         if (firstJob) {
           const allFields = Object.keys(firstJob); // Extract all fields from the first job object
           // Exclude specific fields like "id"
-          const excludedFields = ["id", "logo", "user_id"];
+          const excludedFields = ["id", "logo", "user_id", "work_type"];
           this.searchFields = allFields.filter(
             (field) => !excludedFields.includes(field)
           );
@@ -243,10 +256,20 @@ export default {
         if (this.searchTerm.trim() === "") {
           // If searchTerm is empty, fetch all jobs
           // console.log(`params.filters:`, params.filters);
+
+          if (this.workPlace !== "") {
+            params.filters.work_type = this.workPlace;
+          }
+
           await this.joblist.getJobs(params);
         } else {
           // If searchTerm is not empty, perform search based on searchTerm and searchBy
           // console.log(`Searching = '${this.searchTerm}' by '${this.searchBy}'`);
+
+          if (this.workPlace !== "") {
+            params.filters.work_type = this.workPlace;
+          }
+
           await this.joblist.getJobs({
             ...params,
             searchField: this.searchBy,
@@ -310,8 +333,8 @@ export default {
 
 <style scoped>
 .card {
-  border: 1px solid #ccc;
   border-radius: 5px;
+  border: 0px;
 }
 
 .card-title {
