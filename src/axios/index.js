0dd106335401d7e3a984cf552fe0691d66588d16
hussein-elmlah <husseinelmlah11@gmail.com>
2024-05-17 23:54:34 +0300

    change baseURL

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const base_url = import.meta.env.VITE_base_url;
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: `${base_url}`,
 });
 
 export default axiosInstance;
